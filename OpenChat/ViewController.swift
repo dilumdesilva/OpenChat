764d7bd0529824c3e4e5fc33e0eb006b7bfab142
Dilum De Silva <dilum@hectre.com>
2022-12-16 01:36:33 +1300

    Add initial project

---
+++ OpenChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OpenChat
+//
+//  Created by Dilum De Silva on 2022-12-16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
